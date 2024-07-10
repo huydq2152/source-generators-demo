3f26b1f81ade151309840b28014b613fc80f8380
huydq2152 <quanghuy2152@gmail.com>
2024-07-10 23:35:33 +0700

    init prj

---
+++ WiredBrainCoffee.ConsoleApp/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace WiredBrainCoffee.ConsoleApp.Model
+{
+    public class Person
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"FirstName:{FirstName}; LastName:{LastName}";
+        }
+    }
+}
