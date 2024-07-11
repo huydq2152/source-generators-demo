9fb6c17b0790d67e2b632d1676e3a92b90a03f78
huydq2152 <quanghuy2152@gmail.com>
2024-07-12 01:10:29 +0700

    implement Execute for source generators

--- WiredBrainCoffee.ConsoleApp/Model/Person.cs
+++ WiredBrainCoffee.ConsoleApp/Model/Person.cs
@@ -1,35 +1,34 @@
 ﻿using System.Text;
 
-namespace WiredBrainCoffee.ConsoleApp.Model
+namespace WiredBrainCoffee.ConsoleApp.Model;
+
+public partial class Person
 {
-    public class Person
-    {
-        public string? FirstName { get; set; }
+    public string? FirstName { get; set; }
 
-        public string ? MiddleName { get; set; }
+    public string ? MiddleName { get; set; }
 
-        public string? LastName { get; set; }
+    public string? LastName { get; set; }
 
-        public override string ToString()
-        {
-            var stringBuilder = new StringBuilder();
-            var first = true;
-            foreach (var propertyInfo in GetType().GetProperties())
-            {
-                if(first)
-                {
-                    first = false;
-                }
-                else
-                {
-                    stringBuilder.Append("; ");
-                }
-                var propertyName = propertyInfo.Name;
-                var propertyValue = propertyInfo.GetValue(this);
-                stringBuilder.Append($"{propertyName}: {propertyValue}");
-            }
+    //public override string ToString()
+    //{
+    //    var stringBuilder = new StringBuilder();
+    //    var first = true;
+    //    foreach (var propertyInfo in GetType().GetProperties())
+    //    {
+    //        if(first)
+    //        {
+    //            first = false;
+    //        }
+    //        else
+    //        {
+    //            stringBuilder.Append("; ");
+    //        }
+    //        var propertyName = propertyInfo.Name;
+    //        var propertyValue = propertyInfo.GetValue(this);
+    //        stringBuilder.Append($"{propertyName}: {propertyValue}");
+    //    }
 
-            return stringBuilder.ToString();
-        }
-    }
+    //    return stringBuilder.ToString();
+    //}
 }
--- WiredBrainCoffee.Generators/ToStringGenerator.cs
+++ WiredBrainCoffee.Generators/ToStringGenerator.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace WiredBrainCoffee.Generators;
@@ -11,16 +12,52 @@ public class ToStringGenerator : IIncrementalGenerator
         var classes = context.SyntaxProvider.CreateSyntaxProvider(
             predicate: static (node, _) => node is ClassDeclarationSyntax,
             transform: static (syntaxContext, _) => (ClassDeclarationSyntax)syntaxContext.Node);
-         
-        context.RegisterSourceOutput(classes, 
+
+        context.RegisterSourceOutput(classes,
             static (productionContext, syntax) => Execute(productionContext, syntax));
     }
 
     private static void Execute(SourceProductionContext context, ClassDeclarationSyntax classDeclarationSyntax)
     {
-        var className = classDeclarationSyntax.Identifier.Text;
-        var fileName = $"{className}.g.cs";
+        if (classDeclarationSyntax.Parent is BaseNamespaceDeclarationSyntax namespaceDeclarationSyntax)
+        {
+            var namespaceName = namespaceDeclarationSyntax.Name.ToString();
+            var className = classDeclarationSyntax.Identifier.Text;
+            var fileName = $"{namespaceName}.{className}.g.cs";
 
-        context.AddSource(fileName, $"// Generated!");
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append($@"namespace {namespaceName}
+{{
+    partial class {className}
+    {{
+        public override string ToString()
+        {{
+            return $""");
+            var first = true;
+            foreach (var memberDeclarationSyntax in classDeclarationSyntax.Members)
+            {
+                if (memberDeclarationSyntax is PropertyDeclarationSyntax propertyDeclarationSyntax)
+                {
+                    if (first)
+                    {
+                        first = false;
+                    }
+                    else
+                    {
+                        stringBuilder.Append("; ");
+                    }
+                    var propertyName = propertyDeclarationSyntax.Identifier.Text;
+                    stringBuilder.Append($"{propertyName}: {{{propertyName}}}");
+                }
+            }
+            
+            stringBuilder.Append($@""";
+        }}
+    }}
+}}
+");
+
+            context.AddSource(fileName, stringBuilder.ToString());
+        }
     }
 }
