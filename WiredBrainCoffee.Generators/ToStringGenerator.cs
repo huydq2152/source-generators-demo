918a049d33f4e69f389f60f37e70befe1c335b9f
huydq2152 <quanghuy2152@gmail.com>
2024-07-11 23:54:24 +0700

    add generator project

---
+++ WiredBrainCoffee.Generators/ToStringGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace WiredBrainCoffee.Generators;
+
+[Generator]
+public class ToStringGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var classes = context.SyntaxProvider.CreateSyntaxProvider(
+            predicate: static (node, _) => node is ClassDeclarationSyntax,
+            transform: static (syntaxContext, _) => (ClassDeclarationSyntax)syntaxContext.Node);
+         
+        context.RegisterSourceOutput(classes, 
+            static (productionContext, syntax) => Execute(productionContext, syntax));
+    }
+
+    private static void Execute(SourceProductionContext context, ClassDeclarationSyntax classDeclarationSyntax)
+    {
+        var className = classDeclarationSyntax.Identifier.Text;
+        var fileName = $"{className}.g.cs";
+
+        context.AddSource(fileName, $"// Generated!");
+    }
+}
