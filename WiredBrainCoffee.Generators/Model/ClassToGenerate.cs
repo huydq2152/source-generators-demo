5d282bf12c19bc7e3b1c1d14d1d956c6802faccf
huydq2152 <quanghuy2152@gmail.com>
2024-07-15 20:00:33 +0700

    advoid re generation file every time of key stroke in file

--- WiredBrainCoffee.Generators/Model/ClassToGenerate.cs
+++ WiredBrainCoffee.Generators/Model/ClassToGenerate.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WiredBrainCoffee.Generators.Model;
 
-public class ClassToGenerate
+public class ClassToGenerate : IEquatable<ClassToGenerate?>
 {
     public string NamespaceName { get; }
     public string ClassName { get; }
@@ -13,5 +15,32 @@ public class ClassToGenerate
         NamespaceName = namespaceName;
         ClassName = className;
         PropertyNames = propertyNames;
+    }
+
+    public bool Equals(ClassToGenerate? other)
+    {
+        return !ReferenceEquals(null, other) && 
+               NamespaceName == other.NamespaceName && 
+               ClassName == other.ClassName && 
+               PropertyNames.SequenceEqual(other.PropertyNames);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((ClassToGenerate)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            var hashCode = NamespaceName.GetHashCode();
+            hashCode = (hashCode * 397) ^ ClassName.GetHashCode();
+            hashCode = (hashCode * 397) ^ PropertyNames.GetHashCode();
+            return hashCode;
+        }
     }
 }
